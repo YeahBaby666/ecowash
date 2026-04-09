fa2ad7cfa163edf15a04604894b08b3caa669b7e
YeahBaby666 <maldo7369@gmail.com>
2026-04-09 12:39:41 -0500

    Proyecto: EcoWash Inicialización del proyecto con Spring Boot y Maven

---
+++ src/main/java/utp/edu/pe/ecowash/EcowashApplication.java
@@ -0,0 +1,13 @@
+package utp.edu.pe.ecowash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcowashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcowashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utp/edu/pe/ecowash/EcowashApplicationTests.java
@@ -0,0 +1,13 @@
+package utp.edu.pe.ecowash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcowashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
